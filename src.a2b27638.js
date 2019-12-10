e10319a892c930345810c91e7266fb1c7db4184e
dqunbp <dqunbp@gmail.com>
2019-12-10 12:56:40 +0300

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -836,7 +836,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63390" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53259" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
